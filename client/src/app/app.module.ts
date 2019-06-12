374eaa1428d538b82365dba36cb5342a15df9eea
Juhi22 <j.dani22@freemail.hu>
2019-06-12 14:50:17 +0200

    routing changed

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoggedInComponent } from './components/logged-in/logged-in.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 
 export function HttpLoaderFactory(http: HttpClient) {
@@ -21,7 +22,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     AppComponent,
     LoginComponent,
     HeaderComponent,
-    LoggedInComponent
+    LoggedInComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
