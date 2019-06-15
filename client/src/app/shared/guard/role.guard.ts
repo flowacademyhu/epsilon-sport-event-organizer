98319ae17a57b89213f4332dc478fb95f91b023d
Juhi22 <j.dani22@freemail.hu>
2019-06-15 16:04:35 +0200

    logged in profile can use profile menu

--- client/src/app/shared/guard/role.guard.ts
+++ client/src/app/shared/guard/role.guard.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AppStateService } from '../service/app-state.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleGuard implements CanActivate {
+
+  constructor(
+    private appState: AppStateService
+  ) { }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return ;
+    return this.appState.user.accessToken !== '';
   }
 }
