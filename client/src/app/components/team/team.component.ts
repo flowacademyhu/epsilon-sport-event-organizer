501e93ab529eead9c3580fa15cdfd1e1f816acaa
Juhi22 <j.dani22@freemail.hu>
2019-06-17 15:16:07 +0200

    conflict solved

--- client/src/app/components/team/team.component.ts
+++ client/src/app/components/team/team.component.ts
@@ -10,7 +10,7 @@ export class TeamComponent implements OnInit {
 
   teamName: String = '';
   companyName: String = '';
-  teams: any;
+  teams: any = '';
   creatTeam: Team;
   creatTeamName: String = '';
   creatCompanyName: String = '';
@@ -20,35 +20,19 @@ export class TeamComponent implements OnInit {
   teamNametoAdd: String = '';
   teamtoAddMember: Team;
 
-<<<<<<< HEAD
-  team: any;
-
-=======
->>>>>>> 3e796cb182a11adad9b823b2a14bb12d0030ed19
   constructor(private teamService: TeamService) { }
 
   ngOnInit() {
   }
 
-<<<<<<< HEAD
-  create() {
-    this.team = {name: this.teamName, company: this.companyName, imageUrl: ''};
-
-    this.teamService.create(this.team).subscribe(
-      (data: any) => {
-        console.log(data);
-      }
-    );
-  }
-/* 
-=======
   putMemberInTeam() {
     this.teamtoAddMember = {name: this.teamNametoAdd, company: '', imageUrl: ''};
     this.teamService.putMemberInTeam(this.addMember, this.teamNametoAdd, this.teamtoAddMember).subscribe(
       (data: any) => {
-        console.log('a', data);
+        this.addMember = '';
+        this.teamNametoAdd = '';
       }
-    )
+    );
   }
 
   create() {
@@ -56,35 +40,21 @@ export class TeamComponent implements OnInit {
 
     this.teamService.create(this.creatTeam).subscribe(
       (data: any) => {
-        console.log(data);
         this.data = data;
         this.dataLeader = data.leader;
+        this.creatTeamName = '';
+        this.creatCompanyName = '';
       }
     );
   }
 
->>>>>>> 3e796cb182a11adad9b823b2a14bb12d0030ed19
   getByTeamName() {
     this.teamService.getByTeamName(this.teamName).subscribe(
       (data: Team) => {
         this.teams = data;
-        console.log(data);
+        this.teamName = '';
       }
     );
-<<<<<<< HEAD
-  } */
-
-  /* feri() {
-    this.team.name = this.teamName;
-    this.team.company = this.companyName;
-    this.teamService.update(this.team).subscribe(
-      (team: Team) => {
-        console.log(team);
-      }
-    );
-  } */
-=======
   }
->>>>>>> 3e796cb182a11adad9b823b2a14bb12d0030ed19
 
 }
