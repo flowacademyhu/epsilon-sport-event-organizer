b56ac041c569e837ce7d1e9769319b2219a6c5f8
Tamás Viczián <viczian.t@gmail.com>
2019-06-17 14:49:05 +0200

    repository layer tests added

--- server/src/test/java/hu/flowacademy/epsilon/sport_event_organizer/repository/TeamRepositoryTest.java
+++ server/src/test/java/hu/flowacademy/epsilon/sport_event_organizer/repository/TeamRepositoryTest.java
@@ -1,6 +1,8 @@
 package hu.flowacademy.epsilon.sport_event_organizer.repository;
 
+import hu.flowacademy.epsilon.sport_event_organizer.model.AuthProvider;
 import hu.flowacademy.epsilon.sport_event_organizer.model.Team;
+import hu.flowacademy.epsilon.sport_event_organizer.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Optional;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,6 +26,9 @@ public class TeamRepositoryTest {
     @Autowired
     private TeamRepository teamRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     public void whenFindByName_thanReturnTeam() {
         Team teamToBeFound = new Team();
@@ -32,6 +37,48 @@ public class TeamRepositoryTest {
         testEntityManager.flush();
         Optional<Team> teamFound = teamRepository.findByName(teamToBeFound.getName());
         assertThat(teamToBeFound.getName()).isEqualTo(teamFound.map(Team::getName).orElse(null));
+    }
+
+    @Test
+    public void whenUserSearched_thanReturnUsersMemberTeams() {
+        User user = new User();
+        user.setProvider(AuthProvider.google);
+        Team team1 = new Team();
+        Team team2 = new Team();
+        team1.setName("test eins");
+        team2.setName("test zwei");
+        Set<Team> teamSet = new HashSet<>();
+        teamSet.add(team1);
+        teamSet.add(team2);
+        user.setTeamMember(teamSet);
+        testEntityManager.persist(user);
+        testEntityManager.persist(team1);
+        testEntityManager.persist(team2);
+        testEntityManager.flush();
+        List<Team> teamListFound = teamRepository.findByUsers(user);
+        Set<Team> teamConverted = new HashSet<>(teamListFound);
+        assertThat(user.getTeamMembers()).isEqualTo(teamConverted);
+    }
+
+    @Test
+    public void whenUserSearched_thanReturnUsersLeaderTeams() {
+        User user = new User();
+        user.setProvider(AuthProvider.google);
+        Team team1 = new Team();
+        Team team2 = new Team();
+        team1.setName("test eins");
+        team2.setName("test zwei");
+        Set<Team> teamSet = new HashSet<>();
+        teamSet.add(team1);
+        teamSet.add(team2);
+        user.setTeamLeader(teamSet);
+        testEntityManager.persist(user);
+        testEntityManager.persist(team1);
+        testEntityManager.persist(team2);
+        testEntityManager.flush();
+        List<Team> teamListFound = teamRepository.findByLeaders(user);
+        Set<Team> teamConverted = new HashSet<>(teamListFound);
+        assertThat(user.getTeamLeaders()).isEqualTo(teamConverted);
     }
     
 }
