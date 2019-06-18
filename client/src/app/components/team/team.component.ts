b17a2f9e83b100b213f87078f89d053157157712
Juhi22 <j.dani22@freemail.hu>
2019-06-18 11:50:26 +0200

    delete button

--- client/src/app/components/team/team.component.ts
+++ client/src/app/components/team/team.component.ts
@@ -24,7 +24,6 @@ export class TeamComponent implements OnInit {
   teamNametoDelete: String = '';
 
   isLeader: boolean = false;
-  leadersArray: any[];
 
   constructor(private teamService: TeamService, private state: AppStateService) { }
 
