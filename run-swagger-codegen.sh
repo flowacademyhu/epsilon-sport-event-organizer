47c6c5073d244462485029705fc2221317bcacf7
Boros Robert <robbie13.boros@gmail.com>
2019-06-18 20:56:45 +0200

    adding gitignore to root and update swagger-generator version

--- run-swagger-codegen.sh
+++ run-swagger-codegen.sh
@@ -18,9 +18,9 @@ java -version 2>/dev/null || exit
 
 curl ${SWAGGER_URL} -s || (echo "Server not running or url is invalid!" && exit)
 
-if [ ! -f ./swagger-codegen-cli-2.2.1.jar ]; then
-  wget https://oss.sonatype.org/content/repositories/releases/io/swagger/swagger-codegen-cli/2.2.1/swagger-codegen-cli-2.2.1.jar
+if [ ! -f ./swagger-codegen-cli-2.4.5.jar ]; then
+  wget https://oss.sonatype.org/content/repositories/releases/io/swagger/swagger-codegen-cli/2.4.5/swagger-codegen-cli-2.4.5.jar
 fi
 
-java -jar ./swagger-codegen-cli-2.2.1.jar generate -l typescript-angular2 -i ${SWAGGER_URL} -o ./client/src/app/api
+java -jar ./swagger-codegen-cli-2.4.5.jar generate -l typescript-angular --additional-properties=ngVersion=6.0.0 -i ${SWAGGER_URL} -o ./client/src/app/api
 
