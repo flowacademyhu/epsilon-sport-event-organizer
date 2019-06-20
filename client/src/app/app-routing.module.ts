e94688ee3af7dbbb722df7fc51b63981c1ffde87
micsikz <micsikzoltan@vipmail.hu>
2019-06-20 14:19:34 +0200

    swagger

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 import { MainPageComponent } from './components/main-page/main-page.component';
@@ -11,7 +10,6 @@ import { CupComponent } from './components/cup/cup.component';
 
 const routes: Routes = [
   {path: '', component: MainPageComponent},
-  {path: 'login', component: LoginComponent},
   {path: 'oauth2/redirect', component: MainPageComponent},
   {path: 'profile', component: ProfileComponent, canActivate: [LoggedinGuard]},
   {path: 'team', component: TeamComponent},
--- client/src/app/shared/service/team-state.service.ts
+++ client/src/app/shared/service/team-state.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Team } from 'src/app/api';
 
 @Injectable({
   providedIn: 'root'
