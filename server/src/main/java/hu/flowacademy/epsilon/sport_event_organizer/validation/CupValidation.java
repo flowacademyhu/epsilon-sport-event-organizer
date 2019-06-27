ddb1b0625a7aac200de27f3a460db82dee09b680
simondanielzoltan <badsimon@hotmail.hu>
2019-06-27 16:08:08 +0200

    add cup validations

--- server/src/main/java/hu/flowacademy/epsilon/sport_event_organizer/validation/CupValidation.java
+++ server/src/main/java/hu/flowacademy/epsilon/sport_event_organizer/validation/CupValidation.java
@@ -6,12 +6,17 @@ import hu.flowacademy.epsilon.sport_event_organizer.repository.CupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class CupValidation {
 
     public static final String CUP_IS_ALREADY_EXIST_WITH_THIS_NAME = "validation.cup.already_exist";
     public static final String EVENT_DATE_IS_MISSING = "validation.cup.event_date_missing";
     public static final String REGISTRATION_DATE_IS_MISSING = "validation.cup.reg_date_missing";
+    public static final String REGISTRATION_DATE_IS_AFTER_THE_EVENT = "validation.cup.reg_date_is_after_event";
+    public static final String REGISTRATION_DATE_IS_IN_THE_PAST = "validation.cup.reg_date_is_in_the_past";
+    public static final String EVENT_DATE_IS_IN_THE_PAST = "validation.cup.event_date_is_in_the_past";
 
     @Autowired
     private CupRepository cupRepository;
@@ -28,7 +33,19 @@ public class CupValidation {
         if (cupToSave.getRegistrationEndDate() == null) {
             throw new ValidationException(REGISTRATION_DATE_IS_MISSING);
         }
+        if (cupToSave.getRegistrationEndDate().isAfter(cupToSave.getEventDate())) {
+            throw new ValidationException(REGISTRATION_DATE_IS_AFTER_THE_EVENT);
+        }
+        if (cupToSave.getRegistrationEndDate().isBefore(LocalDate.now())) {
+            throw new ValidationException(REGISTRATION_DATE_IS_IN_THE_PAST);
+        }
+        if (cupToSave.getEventDate().isBefore(LocalDate.now())) {
+            throw new ValidationException(EVENT_DATE_IS_IN_THE_PAST);
+        }
+
+
     }
+
 
     public void validateCupOrganizerBeforeSave(Cup cupToSave) {
         if (cupToSave.getOrganizers().size() == 0) {
@@ -36,9 +53,4 @@ public class CupValidation {
         }
     }
 
-    public void validateApplyTeam(String cupName, String teamName) {
-        if (cupRepository.findByName(cupName).isEmpty()) {
-            throw new ValidationException("Cup is missing");
-        }
-    }
 }
