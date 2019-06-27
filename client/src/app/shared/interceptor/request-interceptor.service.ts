275b11021592acdb0dd31addc9911af4b91f210e
simondanielzoltan <badsimon@hotmail.hu>
2019-06-27 12:30:48 +0200

    init

--- client/src/app/shared/interceptor/request-interceptor.service.ts
+++ client/src/app/shared/interceptor/request-interceptor.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class RequestInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private _snackBar: MatSnackBar,
+    private translate: TranslateService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -16,6 +20,15 @@ export class RequestInterceptorService implements HttpInterceptor {
         }
       });
     }
-    return next.handle(req);
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 400) {
+          this.translate.get(error.error).subscribe((translatedValue) => {
+            this._snackBar.open(translatedValue);
+          });
+        }
+        return throwError(error);
+      })
+    );
   }
 }
