6f1eff5c81a97f554e527d9a3f19c72e3122bc8f
simondanielzoltan <badsimon@hotmail.hu>
2019-06-27 15:55:34 +0200

    add team validations

--- server/src/main/java/hu/flowacademy/epsilon/sport_event_organizer/validation/TeamValidation.java
+++ server/src/main/java/hu/flowacademy/epsilon/sport_event_organizer/validation/TeamValidation.java
@@ -2,7 +2,9 @@ package hu.flowacademy.epsilon.sport_event_organizer.validation;
 
 import hu.flowacademy.epsilon.sport_event_organizer.exception.ValidationException;
 import hu.flowacademy.epsilon.sport_event_organizer.model.Team;
+import hu.flowacademy.epsilon.sport_event_organizer.model.User;
 import hu.flowacademy.epsilon.sport_event_organizer.repository.TeamRepository;
+import hu.flowacademy.epsilon.sport_event_organizer.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,18 @@ public class TeamValidation {
     public static final String TEAM_IS_ALREADY_EXIST_WITH_THIS_NAME = "validation.team.team_name_already_exist";
     public static final String TEAM_NAME_IS_MISSING = "validation.team.team_name_missing";
     public static final String TEAM_COMPANY_NAME_IS_MISSING = "validation.team.team_company_missing";
+    public static final String USER_IS_ALREADY_A_MEMBER = "validation.team.user_is_already_a_member";
+    public static final String USER_EMAIL_IS_MISSING = "validate.team.user_email_is_missing";
+    public static final String USER_EMAIL_IS_FORBIDDEN = "validate.team.user_email_is_forbidden";
+    public static final String USER_NAME_IS_MISSING = "validate.team.user_name_is_missing";
+    public static final String USER_NAME_IS_FORBIDDEN = "validate.team.user_name_is_forbidden";
 
 
     @Autowired
     private TeamRepository teamRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     public void validateTeamNameBeforeSave(Team teamToSave) {
         if (teamRepository.findByName(teamToSave.getName()).isPresent()) {
@@ -30,8 +40,26 @@ public class TeamValidation {
         }
     }
 
-    public void validateUserBeforePutMember(Team userToAddMember) {
+    public void validateUserBeforePutMember(User userToAddMember, Team team) {
+        if (team.getUsers().contains(userToAddMember)) {
+            throw new ValidationException(USER_IS_ALREADY_A_MEMBER);
+        }
+    }
 
+
+    public void validateGuestBeforePutMember(String userName, String email) {
+        if (email.equals("") || email == null) {
+            throw new ValidationException(USER_EMAIL_IS_MISSING);
+        }
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new ValidationException(USER_EMAIL_IS_FORBIDDEN);
+        }
+        if (userName.equals("") || userName == null) {
+            throw new ValidationException(USER_NAME_IS_MISSING);
+        }
+        if (userRepository.findByGoogleName(userName).isPresent()) {
+            throw new ValidationException(USER_NAME_IS_FORBIDDEN);
+        }
 
     }
 
