57729965a8808e585dcbce0947be8812a122e90f
Juhi22 <j.dani22@freemail.hu>
2019-06-28 06:52:37 +0200

    profile functions are added

--- client/src/app/components/profile/profile.component.ts
+++ client/src/app/components/profile/profile.component.ts
@@ -1,18 +1,35 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AppStateService } from 'src/app/shared/service/app-state.service';
 import { MatTableDataSource, MatSort, MatPaginator, MatDialog, MatDialogConfig, } from '@angular/material';
-import { User, TeamResourceService, CupResourceService } from 'src/app/api';
+import { User, TeamResourceService, CupResourceService, Team, Cup } from 'src/app/api';
+import { AddMemberModalComponent } from 'src/app/shared/component/add-member-modal/add-member-modal.component';
+import { DeleteTeamConfirmComponent } from 'src/app/shared/component/delete-team-confirm/delete-team-confirm.component';
+import { ApplyCupModalComponent } from 'src/app/shared/component/apply-cup-modal/apply-cup-modal.component';
+import { DeleteCupConfirmComponent } from 'src/app/shared/component/delete-cup-confirm/delete-cup-confirm.component';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { DeleteLeaderConfirmComponent } from 'src/app/shared/component/delete-leader-confirm/delete-leader-confirm.component';
+import { DeleteMemberConfirmComponent } from 'src/app/shared/component/delete-member-confirm/delete-member-confirm.component';
+import { ApproveCupConfirmComponent } from 'src/app/shared/component/approve-cup-confirm/approve-cup-confirm.component';
+import { DisapproveCupConfirmComponent } from 'src/app/shared/component/disapprove-cup-confirm/disapprove-cup-confirm.component';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class ProfileComponent implements OnInit {
 
   constructor(private state: AppStateService,
               private teamService: TeamResourceService,
-              private cupService: CupResourceService) { }
+              private cupService: CupResourceService,
+              private dialog: MatDialog) { }
 
   userDatas: User;
 
@@ -20,11 +37,13 @@ export class ProfileComponent implements OnInit {
   teamsIAmLeaderInSize: number;
   teamsIAmMemberIn: MatTableDataSource<any>;
   teamsIAmMemberInSize: number;
+  myTeamsDefinition: string[] = ['name', 'company', 'actions'];
   teamsDefinition: string[] = ['name', 'company'];
   listCupData: MatTableDataSource<any>;
   listCupDataSize: number;
   listCupDataMembership: MatTableDataSource<any>;
   listCupDataMembershipSize: number;
+  myDisplayedColumns: string[] = ['name',  'company', 'place', 'registrationEndDate', 'eventDate', 'actions'];
   displayedColumns: string[] = ['name',  'company', 'place', 'registrationEndDate', 'eventDate'];
 
   @ViewChild(MatSort) sort: MatSort;
@@ -37,6 +56,120 @@ export class ProfileComponent implements OnInit {
     this.getTeamsAsMember();
     this.getCupsOrganizer();
     this.getCupsMembers();
+  }
+
+  applyCup(cup: Cup) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '40%';
+    dialogConfig.height = '80%';
+    dialogConfig.data = {cup: cup};
+    this.dialog.open(ApplyCupModalComponent, dialogConfig).afterClosed().subscribe(
+      result => {
+        this.getCupsOrganizer();
+     }
+    );
+  }
+
+  deleteCup(cup: Cup) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '40%';
+    dialogConfig.data = {cup: cup};
+    this.dialog.open(DeleteCupConfirmComponent, dialogConfig).afterClosed().subscribe(
+      result => {
+        this.getCupsOrganizer();
+      }
+    );
+  }
+
+  approveCup(team: Team, cup: Cup) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '40%';
+    dialogConfig.data = {team: team, cup: cup};
+    this.dialog.open(ApproveCupConfirmComponent, dialogConfig).afterClosed().subscribe(
+      result => {
+        this.getCupsOrganizer();
+     }
+    );
+  }
+
+  noApproveCup(team: Team, cup: Cup) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '40%';
+    dialogConfig.data = {team: team, cup: cup};
+    this.dialog.open(DisapproveCupConfirmComponent, dialogConfig).afterClosed().subscribe(
+      result => {
+        this.getCupsOrganizer();
+     }
+    );
+  }
+
+  onAdd(leader: User, team: Team) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '60%';
+    dialogConfig.data = {leader: leader, team: team};
+    this.dialog.open(AddMemberModalComponent, dialogConfig).afterClosed().subscribe(
+      result => {
+        this.getTeamsAsLeader();
+      }
+    );
+  }
+
+  promoteMember(googleName: string, teamName: string) {
+    this.teamService.putLeaderUsingPUT(googleName, teamName).subscribe(
+      (data: any) => {
+        this.getTeamsAsLeader();
+      }
+    );
+
+  }
+
+  deleteMember(member: User, team: Team) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '40%';
+    dialogConfig.data = {member: member, team: team};
+    this.dialog.open(DeleteMemberConfirmComponent, dialogConfig).afterClosed().subscribe(
+      result => {
+        this.getTeamsAsLeader();
+      }
+    );
+  }
+
+  deleteLeader(leader: User, team: Team) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '40%';
+    dialogConfig.data = {leader: leader, team: team};
+    this.dialog.open(DeleteLeaderConfirmComponent, dialogConfig).afterClosed().subscribe(
+      result => {
+        this.getTeamsAsLeader();
+      }
+    );
+  }
+
+  deleteTeam(team: Team) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '40%';
+    dialogConfig.data = {team: team};
+    this.dialog.open(DeleteTeamConfirmComponent, dialogConfig).afterClosed().subscribe(
+      result => {
+        this.getTeamsAsLeader();
+      }
+    );
   }
 
   getTeamsAsLeader() {
@@ -69,7 +202,6 @@ export class ProfileComponent implements OnInit {
             };
           }
         );
-        console.log(teams);
         this.teamsIAmMemberIn = new MatTableDataSource(array);
         this.teamsIAmMemberIn.sort = this.sort;
         this.teamsIAmMemberIn.paginator = this.paginator;
